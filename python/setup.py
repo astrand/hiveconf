cb3c130990f6abe1c8e9f8ab394719bd81ea0749
Peter Åstrand <astrand@lysator.liu.se>
2003-06-10 14:24:39 +0000

    Added distutils files

---
+++ python/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+DESCRIPTION = """\
+hiveconf is a configuration framework
+"""
+
+from distutils.core import setup
+
+setup (name = "hiveconf",
+       version = "0.0",
+       license = "LGPL",
+       description = "configuration framework",
+       long_description = DESCRIPTION,
+       author = "Peter Astrand",
+       author_email = "peter@cendio.se",
+       url = "http://www.cendio.se/~peter/hiveconf",
+       py_modules = ["hiveconf"],
+       data_files=[('/usr/bin', ['hivetool'])]
+       )
