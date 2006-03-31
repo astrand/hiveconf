a2b8fa4fe5f06dca03b1efe5d797a50637cc11b6
Peter Åstrand <astrand@lysator.liu.se>
2006-03-31 12:37:06 +0000

    Find site-packages on system with /usr/lib64. Bumped up release.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ for binary in python python2 python2.0 python2.1 python2.2 python2.3 python2.4 p
     if $binary -c "" ; then
         $binary - <<EOF
 import sys
-sitedir = sys.prefix + "/lib/python" + sys.version[:3] + "/site-packages"
-filename = sitedir + "/hiveconf.pth"
+sitedirs = filter(lambda s: s.endswith("site-packages"), sys.path)
+if len(sitedirs) < 1:
+    sys.exit("Unable to find a site-packages directory in sys.path")
+filename = sitedirs[0] + "/hiveconf.pth"
 open(filename, "w").write("/usr/lib/hiveconf\n")
 print "Created", filename
 EOF
