a727aadc1f32396fb2a9e1890e923d5ef09be575
Peter Åstrand <astrand@lysator.liu.se>
2006-04-02 13:16:50 +0000

    Using curly braces for variable expansions.

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ last_working=""
 
 # Create .pth files
 for binary in python python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.5; do
-    if $binary -c "" 2>/dev/null; then
-        last_working=$binary
-        $binary - <<EOF
+    if ${binary} -c "" 2>/dev/null; then
+        last_working=${binary}
+        ${binary} - <<EOF
 import sys
 sitedirs = filter(lambda s: s.endswith("site-packages"), sys.path)
 if len(sitedirs) < 1:
@@ -20,8 +20,8 @@ EOF
 done
 
 # Modify hashbang of hivetool
-if [ $last_working ]; then
-    $last_working - <<EOF
+if [ ${last_working} ]; then
+    ${last_working} - <<EOF
 import sys
 f = open("/usr/bin/hivetool", "r+")
 lines = f.readlines()
