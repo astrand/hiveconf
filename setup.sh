84c49adef7582ad13c08f5beb77cf2f867cda515
Peter Åstrand <astrand@lysator.liu.se>
2006-04-02 13:39:51 +0000

    Try "python" late; not first.
    Don't add /usr/local/bin to PATH. Instead, try the magic binary
    _python_preferred__, which can be symlinked to the Python of choice

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-PATH=$PATH:/usr/local/bin
 last_working=""
 
 # Create .pth files
-for binary in python python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.5; do
+for binary in python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.5 python _python_preferred_; do
     if ${binary} -c "" 2>/dev/null; then
         last_working=${binary}
         ${binary} - <<EOF
@@ -22,10 +21,13 @@ done
 # Modify hashbang of hivetool
 if [ ${last_working} ]; then
     ${last_working} - <<EOF
-import sys
+import sys, os
+binary = sys.executable
+if binary.endswith("_python_preferred_"):
+    binary = os.readlink(binary)
 f = open("/usr/bin/hivetool", "r+")
 lines = f.readlines()
-lines[0] = "#!%s\n" % sys.executable
+lines[0] = "#!%s\n" % binary
 f.seek(0)
 f.writelines(lines)
 f.close()
