0b147b1d5c6fe4d5ac5cf17f86d2fcedc7c45de1
Peter Åstrand <astrand@lysator.liu.se>
2006-04-02 13:15:53 +0000

    Change the hashbang of hivetool to the last working Python binary.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 PATH=$PATH:/usr/local/bin
+last_working=""
+
+# Create .pth files
 for binary in python python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.5; do
     if $binary -c "" 2>/dev/null; then
+        last_working=$binary
         $binary - <<EOF
 import sys
 sitedirs = filter(lambda s: s.endswith("site-packages"), sys.path)
@@ -14,3 +18,16 @@ print "Created", filename
 EOF
     fi
 done
+
+# Modify hashbang of hivetool
+if [ $last_working ]; then
+    $last_working - <<EOF
+import sys
+f = open("/usr/bin/hivetool", "r+")
+lines = f.readlines()
+lines[0] = "#!%s\n" % sys.executable
+f.seek(0)
+f.writelines(lines)
+f.close()
+EOF
+fi
